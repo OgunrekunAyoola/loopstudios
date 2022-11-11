2863c4e81d5ae0fc6ec5b3ce5f0b421ef2f3a2b7
OgunrekunAyoola <oreofeogunrekun@gmail.com>
2022-11-11 18:47:30 +0100

    .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const primaryNav = document.querySelector(".primary-navigation")
 const navToggle = document.querySelector(".mobile-nav-toggle")
 
+
 navToggle.addEventListener("click" , () => {
     const visibility = primaryNav.getAttribute("data-visible")
 
@@ -11,4 +12,5 @@ navToggle.addEventListener("click" , () => {
         primaryNav.setAttribute("data-visible" , false)
         navToggle.setAttribute("aria-expanded" , false)
     }
+
 });
